0671b2c4c6ea629118e88e5dcd656312fa9a9d8e
Karina <karina.steinberg@voco.ee>
2024-05-09 15:24:26 +0300

    Update article data

--- routes/article.js
+++ routes/article.js
@@ -3,8 +3,9 @@ const router = express.Router()
 const articleController = require('../controllers/article')
 const articleAdminController = require('../controllers/admin/article')
 
-router.get('/', articleController.getAllArticles)
+router.get('/', articleController.getAllArticles);
 router.get('/article/:slug', articleController.getArticleBySlug);
-router.post('/admin/article/create', articleAdminController.createArticle)
+router.post('/admin/article/create', articleAdminController.createArticle);
+router.post('/admin/article/edit/:id', articleAdminController.updateArticle);
 
 module.exports = router;
